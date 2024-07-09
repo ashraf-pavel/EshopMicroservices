2c302766249252352f9aca9273a783728343741e
Ashraful Alam <ashraful.alam@softrobotics.com.bd>
2024-07-09 20:05:11 +0600

    Exception Handling

--- src/services/catalog/CatalogAPI/Products/CreateProduct/CreateProductHandler.cs
+++ src/services/catalog/CatalogAPI/Products/CreateProduct/CreateProductHandler.cs
@@ -17,22 +17,15 @@
 
 
     internal class CreateProductCommandHandler
-        (IDocumentSession session, IValidator<CreateProductCommand> validator)
+        (IDocumentSession session, ILogger<CreateProductCommandHandler> logger)
         : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
         public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
             //Business logic to create a product
-            //validator
-            var result = await validator.ValidateAsync(command, cancellationToken);
-            var errors = result.Errors.Select(x => x.ErrorMessage).ToList();
-
-            if(errors.Any())
-            {
-                throw new ValidationException(errors.FirstOrDefault());
-            }
-
             // Create product entity from command object
+
+            logger.LogInformation("CreateProductCommandHandler.Handle called with {@Command}", command);
 
             var product = new Product
             {
