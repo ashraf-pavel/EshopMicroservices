8efbae389d2b9aacaf0a0f63fb23c86bbc0bcbd1
Ashraful Alam <ashraful.alam@softrobotics.com.bd>
2024-07-09 17:21:03 +0600

    Catalog PUT and DELETE added

--- src/services/catalog/CatalogAPI/GlobalUsing.cs
+++ src/services/catalog/CatalogAPI/GlobalUsing.cs
@@ -4,4 +4,7 @@ global using MediatR;
 global using Marten;
 global using BuildingBlocks.CQRS;
 global using Catalog.API.Modules;
+global using Catalog.API.Exceptions;
+global using FluentValidation;
+
 
--- src/services/catalog/CatalogAPI/Program.cs
+++ src/services/catalog/CatalogAPI/Program.cs
@@ -1,3 +1,5 @@
+using BuildingBlocks.Behaviors;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
@@ -5,7 +7,10 @@ builder.Services.AddCarter();
 builder.Services.AddMediatR(config =>
 {
     config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
 });
+
+builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly);
 
 builder.Services.AddMarten(opts =>
 {
